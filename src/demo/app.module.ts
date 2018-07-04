f87c3cc89789e4203d86416f8278a8ca3f92e265
Corinne Krych <corinnekrych@gmail.com>
2018-07-04 13:11:55 +0200

    feat(demo): initial commit for demo app

--- src/demo/app.module.ts
+++ src/demo/app.module.ts
@@ -8,8 +8,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // App components
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { FABRIC8_TOGGLES_API_URL } from './shared/toggles-api-url';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { FeatureFlagModule } from '../app/feature-flag.module';
+import { FABRIC8_FEATURE_TOGGLES_API_URL, FeatureTogglesService } from '../app/service/feature-toggles.service';
+import { AUTH_API_URL, AuthenticationService, REALM, SSO_API_URL } from 'ngx-login-client';
+import { Broadcaster } from 'ngx-base';
 
 @NgModule({
   imports: [
@@ -17,14 +20,21 @@ import { WelcomeComponent } from './welcome/welcome.component';
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    FeatureFlagModule
   ],
   declarations: [
     AppComponent,
     WelcomeComponent
   ],
   providers: [
-    { provide: FABRIC8_TOGGLES_API_URL, useValue: 'https://toggles.api.prod-preview.openshift.io' }
+    {provide: AUTH_API_URL, useValue: 'https://auth.prod-preview.openshift.io/api/'},
+    {provide: SSO_API_URL, useValue: 'https://sso.prod-preview.openshift.io/api/'},
+    {provide: REALM, useValue: ''},
+    {provide: FABRIC8_FEATURE_TOGGLES_API_URL, useValue: 'https://api.prod-preview.openshift.io/api/'},
+    Broadcaster,
+    AuthenticationService,
+    FeatureTogglesService
   ],
   bootstrap: [AppComponent]
 })
