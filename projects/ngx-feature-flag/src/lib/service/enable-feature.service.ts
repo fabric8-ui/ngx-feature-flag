64ff55117212c7d58966551774c8d38370d7fa33
Divyanshi Gupta <divyanshigupta3295@gmail.com>
2019-01-02 16:53:41 +0530

    fix(feature-warning): add service for enabling feature

---
+++ projects/ngx-feature-flag/src/lib/service/enable-feature.service.ts
@@ -0,0 +1,109 @@
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders
+} from "@angular/common/http";
+import { Inject, Injectable, OnDestroy } from "@angular/core";
+import { cloneDeep } from "lodash";
+import { Logger } from "ngx-base";
+import {
+  AuthenticationService,
+  Profile,
+  User,
+  UserService
+} from "ngx-login-client";
+import {
+  ConnectableObservable,
+  Observable,
+  Subscription,
+  throwError as observableThrowError
+} from "rxjs";
+import { catchError, map, publish } from "rxjs/operators";
+import { FABRIC8_FEATURE_TOGGLES_API_URL } from "./feature-toggles.service";
+
+interface ExtUserResponse {
+  data: ExtUser;
+}
+
+export class ExtUser extends User {
+  attributes: ExtProfile;
+}
+
+export class ExtProfile extends Profile {
+  contextInformation: any;
+  registrationCompleted: boolean;
+  featureLevel: string;
+}
+
+@Injectable()
+export class EnableFeatureService implements OnDestroy {
+  private headers = new HttpHeaders({ "Content-Type": "application/json" });
+  protected subscriptions: Subscription[] = [];
+  private usersUrl: string;
+
+  constructor(
+    protected auth: AuthenticationService,
+    protected http: HttpClient,
+    protected logger: Logger,
+    protected userService: UserService,
+    @Inject(FABRIC8_FEATURE_TOGGLES_API_URL) apiUrl: string
+  ) {
+    if (this.auth.getToken() != undefined) {
+      this.headers = this.headers.set(
+        "Authorization",
+        "Bearer " + this.auth.getToken()
+      );
+    }
+    this.usersUrl = apiUrl + "users";
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => {
+      sub.unsubscribe();
+    });
+  }
+
+  createTransientProfile(): ExtProfile {
+    let profile: ExtUser;
+
+    (this.userService.loggedInUser.pipe(
+      map(
+        (user: User): void => {
+          profile = cloneDeep(user) as ExtUser;
+          if (profile.attributes !== undefined) {
+            profile.attributes.contextInformation =
+              (user as ExtUser).attributes.contextInformation || {};
+          }
+        }
+      ),
+      publish()
+    ) as ConnectableObservable<User>).connect();
+
+    return profile !== undefined && profile.attributes !== undefined
+      ? profile.attributes
+      : ({} as ExtProfile);
+  }
+
+  update(profile: ExtProfile): Observable<ExtUser> {
+    const payload: any = JSON.stringify({
+      data: {
+        attributes: profile,
+        type: "identities"
+      }
+    });
+    return this.http
+      .patch<ExtUserResponse>(this.usersUrl, payload, { headers: this.headers })
+      .pipe(
+        map((response: ExtUserResponse): ExtUser => response.data),
+        catchError(
+          (error: HttpErrorResponse): Observable<ExtUser> =>
+            this.handleError(error)
+        )
+      );
+  }
+
+  protected handleError(error: HttpErrorResponse): Observable<ExtUser> {
+    this.logger.error(error);
+    return observableThrowError(error.message || error);
+  }
+}
--- projects/ngx-feature-flag/src/public_api.ts
+++ projects/ngx-feature-flag/src/public_api.ts
@@ -8,6 +8,7 @@ export { FeatureContainerComponent } from './lib/feature-loader/feature-loader.c
 export { FeatureToggleComponent } from './lib/feature-wrapper/feature-toggle.component';
 
 export { FeatureTogglesService, FABRIC8_FEATURE_TOGGLES_API_URL } from './lib/service/feature-toggles.service';
+export { EnableFeatureService } from './lib/service/enable-feature.service';
 
 export { Feature } from './lib/models/feature';
 export { FeatureFlagConfig } from './lib/models/feature-flag-config';
