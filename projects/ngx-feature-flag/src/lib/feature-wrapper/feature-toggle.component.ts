ef9866f47b04180380c36080a0b83f8f6131e3b4
Divyanshi Gupta <divyanshigupta3295@gmail.com>
2019-01-02 16:55:07 +0530

    fix(feature-toggle): add option to display default/customised warning

--- projects/ngx-feature-flag/src/lib/feature-wrapper/feature-toggle.component.ts
+++ projects/ngx-feature-flag/src/lib/feature-wrapper/feature-toggle.component.ts
@@ -1,23 +1,23 @@
-import {
-  Component,
-  Input,
-  OnInit,
-  TemplateRef
-} from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, Input, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { FeatureTogglesService } from '../service/feature-toggles.service';
+import { Feature } from '../models/feature';
+import { map, catchError, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'f8-feature-toggle',
-  template: `<ng-container [ngTemplateOutlet]="(enabled | async) ? userLevel : defaultLevel"></ng-container>`
+  templateUrl: './feature-toggle.component.html'
 })
 export class FeatureToggleComponent implements OnInit {
+  @ViewChild('featureOptInPage') featureOptInPage: TemplateRef<any>;
 
   @Input() featureName: string;
   @Input() userLevel: TemplateRef<any>;
   @Input() defaultLevel: TemplateRef<any>;
+  @Input() showFeatureOptIn: boolean;
 
-  enabled: Observable<{} | boolean>;
+  feature$: Observable<{} | Feature>;
+  template: TemplateRef<any>;
 
   constructor(private featureService: FeatureTogglesService) {}
 
@@ -25,8 +25,31 @@ export class FeatureToggleComponent implements OnInit {
     if (!this.featureName) {
       throw new Error('Attribute `featureName` should not be null or empty');
     }
-
-    this.enabled = this.featureService.isFeatureUserEnabled(this.featureName);
+    this.feature$ = this.featureService.getFeature(this.featureName).pipe(
+      map((feature: Feature) => {
+        if (feature.attributes) {
+          return {
+            enabled: feature.attributes.enabled && feature.attributes['user-enabled'],
+            level: feature.attributes['enablement-level']
+          };
+        } else {
+          return {};
+        }
+      }),
+      tap((feature) => {
+        if (feature && feature.enabled) {
+          this.template = this.userLevel;
+        } else if (this.showFeatureOptIn) {
+          this.template = this.featureOptInPage;
+        } else {
+          this.template = this.defaultLevel;
+        }
+      }),
+      catchError(() => of({}))
+    );
   }
 
+  setUserLevelTemplate() {
+    this.template = this.userLevel;
+  }
 }
